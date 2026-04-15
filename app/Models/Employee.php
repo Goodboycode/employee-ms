a81abd5c6c58ee8626fe34a4f7966533a41ae1ae
goodboycode <cfapostoljr@gmail.com>
2026-04-15 21:49:59 +0800

    first commit

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'address',
+        'store_id',
+        'position',
+        'is_active',
     ];
+
+    public function store(){
+        return $this->belongsTo(Store::class);
+    }
 }
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -2,14 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'store_id',
         'name',
         'address'
     ];
+
+    public function employees(){    
+        return $this->hasMany(Employee::class);
+    }
         
 }
+
+
