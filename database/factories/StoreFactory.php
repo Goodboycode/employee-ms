d348d0eae0d61744acd2ad0d50ed6c44d7c81490
goodboycode <cfapostoljr@gmail.com>
2026-04-20 22:50:21 +0800

    create seeders

---
+++ database/factories/StoreFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Store;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Store>
+ */
+class StoreFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->randomElement(['NovaMart', 'GreenLeaf Store', 'Urban Basket', 'BrightBuy Shop', 'QuickPick Market', 'Sunrise Goods', 'PrimeChoice Store', 'BlueCart Mart', 'Everyday Essentials', 'Metro Supply Hub']),
+            'address' => $this->faker->city(),
+        ];
+    }
+}
