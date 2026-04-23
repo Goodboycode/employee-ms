211629a66a8e6bc6b4f07801296d4859a4c8223c
goodboycode <cfapostoljr@gmail.com>
2026-04-23 23:54:40 +0800

    fixing the design of search bar

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -16,8 +16,8 @@
     </div>
 
     {{-- Search field --}}
-    <div class="col-md-3 mb-3 d-flex align-items-center">
-        <span class='position-absolute text-muted' style=' z-index:10;'>
+    <div class="col-md-3 mb-3 d-flex align-items-center position-relative">
+        <span class='position-absolute top-50 ms-4 start-10 translate-middle text-muted'>
             <i>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                     class="bi bi-search" viewBox="0 0 16 16">
@@ -26,7 +26,7 @@
                 </svg>
             </i>
         </span>
-        <input id="search-input" onkeyup="searchFunction()" class="form-control ps-5" type="search" placeholder='Search'>
+        <input id="search-input" onkeyup="searchFunction()" class="form-control ps-5" placeholder='Search'>
     </div>
 
     {{-- Check if there are no stores to display --}}
@@ -58,8 +58,21 @@
 
                         <td>
                             {{-- Links to show and edit the store --}}
-                            <a href='{{ route('stores.show', $store->store_id) }}' class='btn btn-info'>Preview</a>
-                            <a href='{{ route('stores.edit', $store->store_id) }}' class='btn btn-primary'>Edit</a>
+                            <a href='{{ route('stores.show', $store->store_id) }}' class='btn btn-info'> <svg
+                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+                                    <path
+                                        d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+                                </svg></a>
+                            <a href='{{ route('stores.edit', $store->store_id) }}' class='btn btn-warning'> <svg
+                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    <path
+                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                    <path fill-rule="evenodd"
+                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                </svg></a>
                         </td>
                     </tr>
                 @endforeach
@@ -74,16 +87,11 @@
             const table = document.querySelector('table');
             const rows = table.getElementsByTagName('tr');
 
-            for (let i = 0; i<rows.length; i++) {
+            for (let i = 0; i < rows.length; i++) {
                 const td = rows[i].getElementsByTagName('td')[1];
                 if (td) {
                     const txtValue = td.textContent || td.innerText;
                     rows[i].style.display = txtValue.toLowerCase().indexOf(filter) > -1 ? '' : 'none';
-                    // if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    //     rows[i].style.display = '';
-                    // } else {
-                    //     rows[i].style.display = 'none';
-                    // }
                 }
 
             }
